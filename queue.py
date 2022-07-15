9a751f984cfcd922142aaa4d5d988eecf4efb860
shreyash59 <108666948+shreyash59@users.noreply.github.com>
2022-07-15 10:22:27 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,29 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+    def display(self):
+        print(self.queue)
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+q.enqueue(5)
+q.enqueue(4)
+q.enqueue(3)
+q.enqueue(2)
+q.enqueue(1)
+
+q.display()
+q.dequeue()
+print("After removing an element")
+q.display()
